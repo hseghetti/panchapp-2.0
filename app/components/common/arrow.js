c641c3a9a0319efdb5ff836388fc41a08b91d109
Javier Baccarelli <Javier.Baccarelli@wnco.com>
2016-08-09 09:36:21 -0500

    Javier - PAN-004 - Creting new components for header and arrow.

---
+++ app/components/common/arrow.js
@@ -0,0 +1,35 @@
+// VENDOR LIBS
+var React = require('react');
+var classNames = require('classnames');
+
+var Arrow = React.createClass({
+
+    getInitialState: function() {
+        return {
+            drawerOpen: true
+        };
+    },
+
+    render: function() {
+        return (
+            <div className={this.getClass()} onClick={this.handleArrowClick}/>
+        );
+    },
+
+    getClass: function () {
+        return classNames({
+            arrow: true,
+            arrow_left: this.state.drawerOpen,
+            arrow_right: !this.state.drawerOpen
+        });
+    },
+
+    handleArrowClick: function () {
+        this.setState({
+            drawerOpen: !this.state.drawerOpen
+        });
+    }
+
+});
+
+module.exports = Arrow;
