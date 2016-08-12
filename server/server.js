618b266ed3d3dbc815e71384d1db0632a21c77a5
Javier Baccarelli <Javier.Baccarelli@wnco.com>
2016-08-12 16:22:07 -0500

    Javier - PAN-008 - fixin array order and adding server response to deep linking.

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, "../app/dist")));
 
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, '../app/dist', 'index.html'));
+})
+
 app.listen(3000, function(){
     console.log("Started listening on port", 3000);
 })
